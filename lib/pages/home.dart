781010b9e6030ca256ed0e84d3a087ab92d6ebda
Henry Man <henryman@Henrys-MacBook-Pro.local>
2023-12-05 14:09:15 +1300

    Completed app bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,7 +7,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
         title: Text(
           'Breakfast',
           style: TextStyle(
@@ -16,18 +21,36 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          child: SvgPicture.asset(
-            'assets/icons/Arrow - Left 2.svg',
-            height: 20,
-            width: 20,
-          ),
-          decoration: BoxDecoration(
-              color: Colors.lightBlue, borderRadius: BorderRadius.circular(10)),
-        ),
-      ),
-    );
+        leading: GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              child: SvgPicture.asset(
+                'assets/icons/Arrow - Left 2.svg',
+                height: 20,
+                width: 20,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(10)),
+            )),
+        actions: [
+          GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                child: SvgPicture.asset(
+                  'assets/icons/dots.svg',
+                  height: 5,
+                  width: 5,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.lightBlue,
+                    borderRadius: BorderRadius.circular(10)),
+              ))
+        ],
+      )
   }
 }
